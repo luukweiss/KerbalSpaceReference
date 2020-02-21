e1ad671fe51cd8276fe6e32c7a2af28e76d6ab34
Luuk <luuk.weiss@takeaway.com>
2020-02-21 12:21:37 +0100

    Fixed bug in main activity

--- app/src/main/java/nl/weiss/kerbalspacereferenceguide/MainActivity.java
+++ app/src/main/java/nl/weiss/kerbalspacereferenceguide/MainActivity.java
@@ -48,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
@@ -60,10 +60,10 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+//    @Override
+//    public boolean onSupportNavigateUp() {
+////        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+////        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+////                || super.onSupportNavigateUp();
+//    }
 }
